bf70b395242e87c686407c6ae1d4bc61a2b09b55
vythili6 <yo.vythilingum@mail.utoronto.ca>
2025-11-20 22:34:53 -0500

    implemented the delete functions and now implementing the update functions

--- school_server.c
+++ school_server.c
@@ -14,7 +14,7 @@ typedef struct student {
     int std_age;
     char std_name[50];
     char std_program[5];
-    int deleted;
+    int deleted; // 0 for not deleted, 1 for deleted
 } Student;
 
 // Function prototypes
@@ -25,6 +25,8 @@ Student *StudentsInProgram(char *program, int *student_count);
 int TotalStudents();
 int DeleteStudentById(int student_id);
 int DeleteStudentByName(char *student_name);
+int UpdateStudentId(int student_id, int new_student_id);
+int UpdateStudentName(char *student_name, char *new_name);
 
 
 
@@ -55,7 +57,7 @@ int main() {
     }
 
     struct sockaddr_in client_addr;
-    unsigned int client_sock_length = sizeof(client_addr);
+    socklen_t client_sock_length = sizeof(client_addr);
     client_addr.sin_family = AF_INET;
     int connection = accept(listen_socket, (struct sockaddr *)&client_addr, &client_sock_length);
     if (connection == -1) {
@@ -103,7 +105,7 @@ int AddStudent (int student_number, char *student_name, int student_age, char *p
 }
 
 int FindStudentId(int student_number, Student *alloc_mem) {
-    // returns 0 on success and 1 on failure
+    // returns 0 on success and 1 on failure and 2 if no student
     // stores the retrieved student data in alloc_mem which is heap memory that was pre-emptively
     // allocated before FindStudent was called
     FILE *file_ptr = fopen("student_record.dat", "rb");
@@ -114,17 +116,17 @@ int FindStudentId(int student_number, Student *alloc_mem) {
 
     Student temp;
     while (fread(&temp, sizeof(Student), 1, file_ptr) == 1) {
-        if ((temp.std_number == student_number) && (temp.deleted != 0)) {
+        if ((temp.std_number == student_number) && (temp.deleted == 0)) {
             *alloc_mem = temp;
             return 0;
         }
     }
     fclose(file_ptr);
-    return 1;
+    return 2;
 }
 
 int FindStudentName(char *student_name, Student *mem_alloc) {
-    // returns 0 on success and 1 on failure
+    // returns 0 on success and 1 on failure and 2 on no student found
     // mem_alloc is pointer to address allocated on heap to store correct student struct
     FILE *file_ptr = fopen("student_record.dat", "rb");
     if (file_ptr == NULL) {
@@ -133,13 +135,13 @@ int FindStudentName(char *student_name, Student *mem_alloc) {
     }
     Student temp;
     while (fread(&temp, sizeof(Student), 1, file_ptr) == 1) {
-        if (strcmp(temp.std_name, student_name) == 0 && temp.deleted != 0) {
+        if (strcmp(temp.std_name, student_name) == 0 && temp.deleted == 0) {
             *mem_alloc = temp;
             return 0;
         }
     }
     fclose(file_ptr);
-    return 1;
+    return 2;
 }
 
 Student *StudentsInProgram(char *program, int *student_count) {
@@ -156,7 +158,7 @@ Student *StudentsInProgram(char *program, int *student_count) {
     }
     Student temp;
     while (fread(&temp, sizeof(Student), 1, file_ptr) == 1) {
-        if (strcmp(temp.std_program, program) == 0 && temp.deleted != 0) {
+        if (strcmp(temp.std_program, program) == 0 && temp.deleted == 0) {
             set_count++;
         }
     }
@@ -173,7 +175,7 @@ Student *StudentsInProgram(char *program, int *student_count) {
         for (i = 0; i < set_count; i++) {
             Student temp2;
             if (fread(&temp2, sizeof(Student), 1, file_ptr) == 1) {
-                if (strcmp(temp2.std_program, program) == 0 && temp2.deleted != 0) {
+                if (strcmp(temp2.std_program, program) == 0 && temp2.deleted == 0) {
                     heap_ptr[i] = temp2;
                 }
             }
@@ -194,7 +196,7 @@ int TotalStudents() {
     int total = 0;
     Student some_temp;
     while (fread(&some_temp, sizeof(Student), 1, file_ptr) == 1) {
-        if (some_temp.deleted != 0) {
+        if (some_temp.deleted == 0) {
             total++;
         }
     }
@@ -202,9 +204,113 @@ int TotalStudents() {
 }
 
 int DeleteStudentById(int student_id) {
-    // returns 1 if deletion successful and returns 0 otherwise.
+    // returns 0 if deletion successful and returns 1 otherwise.
     // change the delete flag by overwriting it at that specific position
     // dont forget to open file in a binary read and write mode if thats possible.
+    FILE *file_ptr = fopen("student_record.dat", "rb+");
+    if (file_ptr == NULL) {
+        perror("error opening the file\n");
+        return 1;
+    }
+    Student temp;
+    while (fread(&temp, sizeof(Student), 1, file_ptr) == 1) {
+        if (temp.std_number == student_id && temp.deleted == 0) {
+            temp.deleted = 1;
+            fseek(file_ptr, -(sizeof(Student)), SEEK_CUR);
+            size_t modified_student_written = fwrite(&temp, sizeof(Student), 1, file_ptr);
+            if (modified_student_written == -1) {
+                perror("Could not overwite the student data\n");
+                fclose(file_ptr);
+                return 1;
+            }
+            fclose(file_ptr);
+            return 0;
+        } else if (temp.std_number == student_id && temp.deleted == 1) {
+            printf("Student already deleted\n");
+            return 1;
+        }
+    }
+    fclose(file_ptr);
+    return 1;
+
+
+}
+
+int DeleteStudentByName(char *student_name) {
+    // same as delete student by id except it deletes the student record as per their name
+    FILE *file_ptr = fopen("student_record.dat", "rb+");
+    if (file_ptr == NULL) {
+        perror("Error opening the file\n");
+        return 1;
+    }
+    Student temp;
+    while (fread(&temp, sizeof(Student), 1, file_ptr) == 1) {
+        if (strcmp(temp.std_name, student_name) == 0 && temp.deleted == 0) {
+            temp.deleted = 1;
+            fseek(file_ptr, -(sizeof(Student)), SEEK_CUR);
+            size_t modified_student = fwrite(&temp, sizeof(Student), 1, file_ptr);
+            if (modified_student == -1) {
+                perror("Couldnt overwrite the student data\n");
+                fclose(file_ptr);
+                return 1;
+            }
+            fclose(file_ptr);
+            return 0;
+        } else if (strcmp(temp.std_name, student_name) == 0 && temp.deleted == 1) {
+            printf("Student already deleted\n");
+            return 1;
+        }
+    }
+    fclose(file_ptr);
+    return 1;
+}
+
+int UpdateStudentId(int student_id, int new_student_id) {
+    // return 0 on success and 1 on a failure
+    FILE *file_ptr = fopen("student_record.dat", "rb+");
+    if (file_ptr == NULL) {
+        perror("Error on opening file\n");
+        return 1;
+    }
+    Student temp;
+    while (fread(&temp, sizeof(Student), 1, file_ptr) == 1) {
+        if (temp.std_number == student_id && temp.deleted == 0) {
+            temp.std_number = new_student_id;
+            fseek(file_ptr, -(sizeof(Student)), SEEK_CUR);
+            size_t modified_student = fwrite(&temp, sizeof(Student), 1, file_ptr);
+            if (modified_student == -1) {
+                perror("couldnt overwrite the student data\n");
+                fclose(file_ptr);
+                return 1;
+            }
+            fclose(file_ptr);
+            return 0;
+        } else if (temp.std_number == student_id && temp.deleted == 1) {
+            printf("Student could not be updated as record has been deleted\n");
+            return 1;
+        }
+    }
+    fclose(file_ptr);
+    return 1;
+}
+
+int UpdateStudentName(char *student_name, char *new_name) {
+    // return 0 on success and 1 on failure
+    if (strlen(new_name) > 50) {
+        printf("New name is too long\n");
+        return 1;
+    }
+    FILE *file_ptr = fopen("student_record.dat", "rb+");
+    if (file_ptr == NULL) {
+        perror("Error opening the file\n");
+        return 1;
+    }
+    Student temp;
+    while (fread(&temp, sizeof(Student), 1, file_ptr) == 1) {
+        if (strcmp(temp.std_name, student_name) == 0 && temp.deleted == 0) {
+            // copy the new name into the field for the existing student name
+        }
+    }
 }
 
 
